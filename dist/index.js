cef5033383fe99f449fca340305a5ad5c231fc80
Steven Medina <raymond.steven.medina@gmail.com>
2024-04-04 22:09:10 -0400

    Prueba de inicializacion de app

--- dist/index.js
+++ dist/index.js
@@ -6,13 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const database_1 = require("./src/database");
 const dotenv_1 = __importDefault(require("dotenv"));
-const toClients_1 = require("./src/whatsapp/send-messages/toClients");
-const cancellationRequest_1 = require("./src/whatsapp/send-messages/toAdministration/cancellationRequest");
-require("./src/whatsapp/initWhatsapp");
-const dailyClosing_1 = require("./src/whatsapp/send-messages/toAdministration/dailyClosing");
-const weeklyClosing_1 = require("./src/whatsapp/send-messages/toAdministration/weeklyClosing");
-const massiveMessages_1 = require("./src/whatsapp/send-messages/massiveMessages");
-const orderRequest_1 = require("./src/whatsapp/send-messages/toAdministration/orderRequest");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = 3000;
@@ -20,10 +13,12 @@ const port = 3000;
 (0, database_1.initDB)(process.env.DB_HOST);
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
-    toClients_1.sendMessagesClients.start();
-    weeklyClosing_1.sendWeeklyCollectionRecord.start();
-    orderRequest_1.sendOrderRequest.start();
-    dailyClosing_1.sendDailyClosing.start();
-    cancellationRequest_1.sendCancellationsRequest.start();
-    massiveMessages_1.sendMassiveMessages.start();
+    //sendMessagesClients.start()
+    //
+    //sendWeeklyCollectionRecord.start()
+    //sendOrderRequest.start()
+    //sendDailyClosing.start()
+    //
+    //sendCancellationsRequest.start()
+    //sendMassiveMessages.start()
 });
