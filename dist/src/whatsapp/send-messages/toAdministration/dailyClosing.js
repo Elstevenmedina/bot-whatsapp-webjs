8f11cd37ee748ac6c92c48ded77d225d84f066c2
Steven Medina <raymond.steven.medina@gmail.com>
2024-04-08 12:31:08 -0400

    Cambiando numero telefonico

--- dist/src/whatsapp/send-messages/toAdministration/dailyClosing.js
+++ dist/src/whatsapp/send-messages/toAdministration/dailyClosing.js
@@ -7,6 +7,6 @@ const getDailyClosing_1 = require("../../../utils/getDailyClosing");
 const ManagementWhatsappNumbers_1 = require("../../../enums/ManagementWhatsappNumbers");
 exports.sendDailyClosing = new cron_1.CronJob('01 01 21 * * *', async () => {
     const dailyClosing = await (0, getDailyClosing_1.getDailyClosing)();
-    const message = `Monto vendido: ${dailyClosing.amountSold}\nMonto cobrado: ${dailyClosing.amountCollected}\nEgreso por devoluciones: ${dailyClosing.egressReturn}\nEgreso por anulaciones: ${dailyClosing.egressCancellation}\nEgreso general: ${dailyClosing.egressGeneral}\nCuentas por cobrar: ${dailyClosing.accountsCollect} \n\n`;
+    const message = `*Reporte de cierre diario*\n\n *Monto vendido*: ${dailyClosing.amountSold}\*nMonto cobrado*: ${dailyClosing.amountCollected}\*nEgreso por devoluciones*: ${dailyClosing.egressReturn}\*nEgreso por anulaciones*: ${dailyClosing.egressCancellation}\*nEgreso general*: ${dailyClosing.egressGeneral}\*nCuentas por cobrar*: ${dailyClosing.accountsCollect} \n\n`;
     (0, initWhatsapp_1.sendMessage)(ManagementWhatsappNumbers_1.ManagementWhatsappNumbers.Management1, message);
 });
--- src/whatsapp/send-messages/toAdministration/weeklyClosing.ts
+++ src/whatsapp/send-messages/toAdministration/weeklyClosing.ts
@@ -8,10 +8,10 @@ export const sendWeeklyCollectionRecord = new CronJob('01 01 21 * * fri', async 
 
   if (weeklyCollection[0] !== null && weeklyCollection[0] !== undefined) {
     const message = `*Constancia semanal de cobro*\n\n Monto Cobrado :${new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(weeklyCollection[0].PagadoTotal)} \n\n https://app.thomsonparts.com/ver-constancia-cobros/${weeklyCollection[0].Numero}`
-    sendMessage('584244577241@c.us'/*ManagementWhatsappNumbers.Management1*/, message)
+    sendMessage(ManagementWhatsappNumbers.Management1, message)
   }
   if (weeklyCollection[1] !== null && weeklyCollection[1] !== undefined) {
     const message = `*Constancia semanal de cobro*\n\n Monto Cobrado :${new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(weeklyCollection[1].PagadoTotal)} \n\n https://app.thomsonparts.com/ver-constancia-cobros/${weeklyCollection[1].Numero}`
-    sendMessage('584244577241@c.us'/*ManagementWhatsappNumbers.Management2*/, message)
+    sendMessage(ManagementWhatsappNumbers.Management2, message)
   }
 })
